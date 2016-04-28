448b243a76f5368df9136797718ca3ac95ca57b4
Centfocs <zentfocs@gmail.com>
2016-04-28 15:58:18 +0200

    Update exer_1.php

--- exer_1.php
+++ exer_1.php
@@ -3,8 +3,18 @@
 // Prova de codi en GitHub, per saver com funciona
 
 
+function suma ($suma) {
+
+  $suma = $suma + 2;
+  
+  return $suma
+}
 
 
+$a = 4;
 
+$a = $a + suma($a);
+
+print "$a";
 
 ?>
